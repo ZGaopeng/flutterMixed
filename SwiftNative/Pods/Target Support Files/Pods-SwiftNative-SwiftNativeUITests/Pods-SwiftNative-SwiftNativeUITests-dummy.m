b78d2f5dadae6e8f907bc9b9dfcf54a652fcd664
祝高鹏 <zhugaopeng@58.com>
2023-05-24 17:17:02 +0800

    mod: 新建原生工程

---
+++ SwiftNative/Pods/Target Support Files/Pods-SwiftNative-SwiftNativeUITests/Pods-SwiftNative-SwiftNativeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftNative_SwiftNativeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftNative_SwiftNativeUITests
+@end
--- SwiftNative/SwiftNative/ViewController.swift
+++ SwiftNative/SwiftNative/ViewController.swift
@@ -4,16 +4,24 @@
 //
 //  Created by 小瓶子 on 2023/5/24.
 //
-
 import UIKit
-
+import Flutter
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "混合开发测试"
+        let btn: UIButton = UIButton()
+        btn.backgroundColor = .cyan
+        btn.frame = CGRect(x: 50, y: 100, width: 100, height: 50)
+        btn.addTarget(self, action: #selector(btnChoose), for: .touchUpInside)
+        self.view.addSubview(btn);
+        
     }
-
-
+    @objc func btnChoose() {
+        let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
+        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+        present(flutterViewController, animated: true, completion: nil)
+    }
 }
 
