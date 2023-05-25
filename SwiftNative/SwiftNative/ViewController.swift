3937eeb0e5e21d0a6e38d2f7d3393557d2353082
祝高鹏 <zhugaopeng@58.com>
2023-05-25 18:49:23 +0800

    no message

--- SwiftNative/SwiftNative/ViewController.swift
+++ SwiftNative/SwiftNative/ViewController.swift
@@ -10,25 +10,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.title = "混合开发测试"
         let btn: UIButton = UIButton()
-        btn.backgroundColor = .cyan
+        btn.backgroundColor = .blue
         btn.frame = CGRect(x: 50, y: 300, width: 100, height: 50)
         btn.addTarget(self, action: #selector(btnChoose), for: .touchUpInside)
-       
+        btn.setTitle("打开flutter页面", for: .normal)
         self.view.addSubview(btn)
+        
+        
         // flutter作为item嵌入
-        let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
-        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
-        addChild(flutterViewController)
-        flutterViewController.view.translatesAutoresizingMaskIntoConstraints = false
-
-        guard let flutterView = flutterViewController.view else { return }
-
-        flutterView.translatesAutoresizingMaskIntoConstraints = false
-
-        view.addSubview(flutterView)
-        flutterView.frame = CGRect(x: 0, y: 400, width: 414, height: 400)
+//        let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
+//        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+//        addChild(flutterViewController)
+//        flutterViewController.view.translatesAutoresizingMaskIntoConstraints = false
+//
+//        guard let flutterView = flutterViewController.view else { return }
+//
+//        flutterView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        view.addSubview(flutterView)
+//        flutterView.frame = CGRect(x: 0, y: 400, width: 414, height: 400)
 //        let constraints = [
 //            flutterView.topAnchor.constraint(equalTo: btn.bottomAnchor),
 //            flutterView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -44,13 +45,13 @@ class ViewController: UIViewController {
     }
     @objc func btnChoose() {
         // 第一种方式：
-//        let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
-//        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
-//        present(flutterViewController, animated: true, completion: nil)
+        let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
+        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+        present(flutterViewController, animated: true, completion: nil)
         
         // 第二种方式：
-        let flutterVC = FlutterViewController()
-        present(flutterVC, animated: true)
+//        let flutterVC = FlutterViewController()
+//        present(flutterVC, animated: true)
         
     }
 }
